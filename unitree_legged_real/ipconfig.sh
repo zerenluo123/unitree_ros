f1cbb78e83c1789638dc418e75e31aa72c526363
zerenluo <zerluo@student.ethz.ch>
2022-08-16 16:57:11 +0200

    resolve conflict and remove unitree_legged_real

--- unitree_legged_real/ipconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo ifconfig lo multicast
-sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
-
-sudo ifconfig eno1 down
-sudo ifconfig eno1 up 192.168.123.2 netmask 255.255.255.0
-
